06c7ff2bde30ff405e1f7087732e9f8090b4e39b
Eman Curmi <emancurmi@gmail.com>
2021-01-25 16:43:34 -0500

    dbupdate

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@ module.exports = {
     PORT: process.env.PORT || 8137,
     NODE_ENV: process.env.NODE_ENV || 'production',
     API_TOKEN: process.env.API_TOKEN || '7b967bbe-afc2-11ea-b3de-0242ac130004',
-    DB_URL: process.env.DB_URL || 'postgres://edunsyapinytee:ab2164c3f71ab53c3611c73f4288875e26ee84590a16e1af077e4f2fe8c0ba53@ec2-54-156-73-147.compute-1.amazonaws.com:5432/d60plfiabm6hvp',
+    DB_URL: process.env.DB_URL || 'postgres://umzdphqiohirul:a27069a4026105e5c83bcbc04c576441fb0aec73c241c91a45326b310963790c@ec2-52-213-173-172.eu-west-1.compute.amazonaws.com:5432/d5it7csmicrnve',
 }
--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ const { ADDRESS, PORT, DB_URL } = require('./config')
 const db = knex({
     client: 'pg',
     connection: DB_URL,
+    ssl: require
 })
 
 app.set('db', db)
