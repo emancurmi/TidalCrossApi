de2fb3fcd1f681baa0863483931ecee98dd5ee4d
Eman Curmi <emancurmi@gmail.com>
2021-01-25 00:23:34 -0500

    bugfix

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@ module.exports = {
     PORT: process.env.PORT || 8137,
     NODE_ENV: process.env.NODE_ENV || 'production',
     API_TOKEN: process.env.API_TOKEN || '7b967bbe-afc2-11ea-b3de-0242ac130004',
-    DATABASE_URL: process.env.DATABASE_URL || 'postgres://mzncziuayodpkz:5f259572bb0e0fba83a2129763e9a9f1bc5e1b6d4f1bf4507d71184919f49f2f@ec2-100-25-231-126.compute-1.amazonaws.com:5432/d4i3f6ebq1pdjn' + '?ssl=true',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgres://edunsyapinytee:ab2164c3f71ab53c3611c73f4288875e26ee84590a16e1af077e4f2fe8c0ba53@ec2-54-156-73-147.compute-1.amazonaws.com:5432/d60plfiabm6hvp' + '?ssl=true',
 }
