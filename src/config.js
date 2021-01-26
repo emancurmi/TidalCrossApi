51ed7df435543a165f25d6cb24421d1904ee30fa
Eman Curmi <emancurmi@gmail.com>
2021-01-25 22:15:23 -0500

    bugfix

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    ADDRESS: process.env.ADDRESS || 'http://tidalcross.com/',
+    ADDRESS: process.env.ADDRESS || 'https://tidalcrossapi.herokuapp.com/',
     PORT: process.env.PORT || 8137,
     NODE_ENV: process.env.NODE_ENV || 'production',
     API_TOKEN: process.env.API_TOKEN || '7b967bbe-afc2-11ea-b3de-0242ac130004',
-    DB_URL: process.env.DB_URL || 'postgres://umzdphqiohirul:a27069a4026105e5c83bcbc04c576441fb0aec73c241c91a45326b310963790c@ec2-52-213-173-172.eu-west-1.compute.amazonaws.com:5432/d5it7csmicrnve?ssl=true',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgres://umzdphqiohirul:a27069a4026105e5c83bcbc04c576441fb0aec73c241c91a45326b310963790c@ec2-52-213-173-172.eu-west-1.compute.amazonaws.com:5432/d5it7csmicrnve?ssl=true',
 }
