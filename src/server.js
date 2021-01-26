fe41fdc5f5c15fd127c2e43b7f27c4fe0d145faa
Eman Curmi <emancurmi@gmail.com>
2021-01-25 23:22:11 -0500

    server api

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const knex = require('knex')
 const app = require('./app')
-const { ADDRESS, PORT } = require('./config')
+const { ADDRESS, PORT, DATABASE_URL } = require('./config')
 
 const db = knex({
     client: 'pg',
