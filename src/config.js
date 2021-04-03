4faa9871d06dce83172605a53c7a84f08bbb437c
Eman Curmi <emancurmi@gmail.com>
2021-04-03 16:21:07 -0400

    db update

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'production',
     API_TOKEN: process.env.API_TOKEN || '$2a$10$ra1z0n2XnSnbMP/ipTMHeOqqrI7i8Rssm/z8MHTxgb7LamV7LpfXu',
-    DATABASE_URL: process.env.DATABASE_URL,
+    DATABASE_URL: process.env.DB_URL,
 }
