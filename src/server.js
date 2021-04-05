9ce7fe09520bb8f6fedae6de170f068bbd76fb0d
Eman Curmi <emancurmi@gmail.com>
2021-04-04 22:26:47 -0400

    Performance Upgrades

--- src/server.js
+++ src/server.js
@@ -3,8 +3,16 @@ const app = require('./app')
 const { ADDRESS, PORT, DATABASE_URL } = require('./config')
 
 const db = knex({
-    client: 'pg',
-    connection: DATABASE_URL,
+   //client: 'pg',
+   // connection: DATABASE_URL,
+    client: 'mysql',
+    connection: {
+        host: DB_HOST,
+        user: DB_USER,
+        password: DB_PASSWORD,
+        database: DB_NAME
+    },
+    pool: {min:0, max:7}
 })
 
 app.set('db', db)
